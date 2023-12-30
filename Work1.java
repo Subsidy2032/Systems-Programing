9bc0a7755286a921bb760527c92aa596e30589fe
Subsidy2032 <noam133m@gmail.com>
2023-12-30 18:46:56 +0200

    updated code

--- Work1.java
+++ Work1.java
@@ -1,3 +1,6 @@
+// This program gets 2 files, 1 with all students and one with the grades of students in a specific course, and outputs a new file with shared students.
+// Ron Bitan & Noam Muchnik
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.File;
@@ -7,12 +10,17 @@ import java.util.Scanner;
 public class Work1 {
 
 	public static void main(String[] args) {
-		File students = new File("CS.txt");
-		File course = new File("hedva.txt");
+		String studentsFile = "CS.txt";
+		String courseFile = "hedva.txt";
+		
+		// Opening the required files and initializing the scanners and FileWriter
+		File students = new File(studentsFile);
+		File course = new File(courseFile);
 		Scanner studentsScanner = null;
 		Scanner courseScanner = null;
 		FileWriter out = null;
 		
+		// Giving values to the scanners and FileWriter
 		try {
 			studentsScanner = new Scanner(students);
 			courseScanner = new Scanner(course);
@@ -29,41 +37,48 @@ public class Work1 {
 			System.exit(0);
 		}
 		
-		String toWrite;
+		String toWrite; // Lines to write to the output file
 		double avg;
-		String curHedvaLine = studentsScanner.nextLine();
-		String curCSline = courseScanner.nextLine();
+		String curStudentsLine = studentsScanner.nextLine(); // To store the current line from the students file
+		String curCourseLine = courseScanner.nextLine(); // To store the current line from the course file
+		boolean continueLoop = true;
 		
-		while (studentsScanner.hasNextLine() || courseScanner.hasNextLine()) {
-			String CSwords[] = curHedvaLine.split(" ");
-			String hedvaWords[] = curCSline.split(" ");
+		while (continueLoop) {
+			// Splitting the lines to arrays of words
+			String studentsWords[] = curStudentsLine.split(" ");
+			String courseWords[] = curCourseLine.split(" ");
 			
-			int id1 = Integer.parseInt(CSwords[2]);
-			int id2 = Integer.parseInt(hedvaWords[0]);
+			// Turning the ids to integers
+			int id1 = Integer.parseInt(studentsWords[2]);
+			int id2 = Integer.parseInt(courseWords[0]);
 			
+			// If an equal id is found in both files, prints the student name, id and average to the output file
 			if (id1 == id2) {
-				avg = (Integer.parseInt(hedvaWords[1]) + Integer.parseInt(hedvaWords[2])) / 2;
-				toWrite = CSwords[2] + " " + CSwords[0] + " " + CSwords[1] + " " + avg;
+				avg = ((double)Integer.parseInt(courseWords[1]) + (double)Integer.parseInt(courseWords[2])) / 2;
+				toWrite = studentsWords[2] + " " + studentsWords[0] + " " + studentsWords[1] + " " + avg;
 				try {
 					out.write(toWrite + "\n");
 				} catch (IOException e) {
 					System.out.println("IO exception");
 				}
 				if (studentsScanner.hasNextLine())
-					curHedvaLine = studentsScanner.nextLine();
+					curStudentsLine = studentsScanner.nextLine();
 				if (courseScanner.hasNextLine())
-					curCSline = courseScanner.nextLine();
+					curCourseLine = courseScanner.nextLine();
+				if (!(studentsScanner.hasNextLine()) && !(courseScanner.hasNextLine()))
+					continueLoop = false;
 			}
 			
-			else if (id1 < id2) {
-				if (studentsScanner.hasNextLine())
-					curHedvaLine = studentsScanner.nextLine();
+			// Goes to the next line in one of the files (depends on where the id is smaller)
+			else if ((id1 < id2 && studentsScanner.hasNextLine()) || (studentsScanner.hasNextLine() && !(courseScanner.hasNextLine()))) {
+					curStudentsLine = studentsScanner.nextLine();
 			}
 			
-			else {
-				if (courseScanner.hasNextLine())
-					curCSline = courseScanner.nextLine();
-			}
+			else if (courseScanner.hasNextLine())
+					curCourseLine = courseScanner.nextLine();
+			
+			else
+				continueLoop = false;
 		}
 		
 		studentsScanner.close();
