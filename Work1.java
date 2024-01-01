6391f744c71ce24b15194bc6507d50384316bbb4
Subsidy2032 <noam133m@gmail.com>
2024-01-01 20:10:26 +0200

    working as it should

--- Work1.java
+++ Work1.java
@@ -1,6 +1,3 @@
-// This program gets 2 files, 1 with all students and one with the grades of students in a specific course, and outputs a new file with shared students.
-// Ron Bitan & Noam Muchnik
-
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.File;
@@ -12,15 +9,13 @@ public class Work1 {
 	public static void main(String[] args) {
 		String studentsFile = "CS.txt";
 		String courseFile = "hedva.txt";
-		
-		// Opening the required files and initializing the scanners and FileWriter
+
 		File students = new File(studentsFile);
 		File course = new File(courseFile);
 		Scanner studentsScanner = null;
 		Scanner courseScanner = null;
 		FileWriter out = null;
 		
-		// Giving values to the scanners and FileWriter
 		try {
 			studentsScanner = new Scanner(students);
 			courseScanner = new Scanner(course);
@@ -37,22 +32,19 @@ public class Work1 {
 			System.exit(0);
 		}
 		
-		String toWrite; // Lines to write to the output file
+		String toWrite;
 		double avg;
-		String curStudentsLine = studentsScanner.nextLine(); // To store the current line from the students file
-		String curCourseLine = courseScanner.nextLine(); // To store the current line from the course file
+		String curStudentsLine = studentsScanner.nextLine();
+		String curCourseLine = courseScanner.nextLine();
 		boolean continueLoop = true;
 		
 		while (continueLoop) {
-			// Splitting the lines to arrays of words
 			String studentsWords[] = curStudentsLine.split(" ");
 			String courseWords[] = curCourseLine.split(" ");
 			
-			// Turning the ids to integers
 			int id1 = Integer.parseInt(studentsWords[2]);
 			int id2 = Integer.parseInt(courseWords[0]);
 			
-			// If an equal id is found in both files, prints the student name, id and average to the output file
 			if (id1 == id2) {
 				avg = ((double)Integer.parseInt(courseWords[1]) + (double)Integer.parseInt(courseWords[2])) / 2;
 				toWrite = studentsWords[2] + " " + studentsWords[0] + " " + studentsWords[1] + " " + avg;
@@ -69,7 +61,6 @@ public class Work1 {
 					continueLoop = false;
 			}
 			
-			// Goes to the next line in one of the files (depends on where the id is smaller)
 			else if ((id1 < id2 && studentsScanner.hasNextLine()) || (studentsScanner.hasNextLine() && !(courseScanner.hasNextLine()))) {
 					curStudentsLine = studentsScanner.nextLine();
 			}
